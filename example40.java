9e8b11443c02125e4066e31f06e61182bf1135a2
elifsuedaakin <117524345+elifsuedaakin@users.noreply.github.com>
2023-04-04 13:03:09 +0300

    Update example40.java

--- example40.java
+++ example40.java
@@ -1,4 +1,4 @@
-/*
+/*herhangi bir pozitif tam sayının pozitif tam bölenlerinin ortalaması ,yine tam sayı ise bunlara aritmeetik sayı denir örn 6 tamsayısı (1+2+3+6)/4=3
  * klavyeden girilen pozitif tam sayının "aritmetik sayı" olup olmadığını kontrol eden program 
  */
 import java.util.Scanner;
@@ -15,15 +15,15 @@ public class example40 {
         Scanner input=new Scanner(System.in);
         System.out.println("tam sayı girin");
         int a=input.nextInt();
-        int t=0;
+        int toplam=0;
         int s=0;
         for(int i=1;i<=a;i++){
             if(a%i==0){
-                t=t+1;
+                toplam=toplam+i;
                 s=s+1;
             }
         }
-        if(t%s==0){
+        if(toplam%s==0){
             System.out.println("aritmetik sayıdır");
         }
         else 
